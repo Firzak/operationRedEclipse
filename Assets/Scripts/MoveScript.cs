0744e5731c2d1058bf9902a3b436af03b9899dc8
Mounir MEROUANE <mounir.merouane.mm@gmail.com>
2025-08-11 11:11:47 +0200

    .

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,28 +1,29 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class MoveScript : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;         // Vitesse de déplacement du personnage
     
-    private GetInputScript inputScript;
-    private CharacterController controller;  // Composant optionnel pour la collision (peut être null si non utilisé)
+    private GetInputScript _inputScript;
+    private CharacterController _controller;  // Composant optionnel pour la collision (peut être null si non utilisé)
+    private Camera _camera;
 
     void Start()
     {
-        inputScript = GetComponent<GetInputScript>();
-        controller = GetComponent<CharacterController>();  // Facultatif : utilise le CharacterController s'il est présent
+        _camera = Camera.main;
+        _inputScript = GetComponent<GetInputScript>();
+        _controller = GetComponent<CharacterController>();  // Facultatif : utilise le CharacterController s'il est présent
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // Récupère le vecteur de mouvement (Move) du nouveau Input System
-        Vector2 moveInput = inputScript != null ? inputScript.MoveInput : Vector2.zero;
+        Vector2 moveInput = _inputScript ? _inputScript.MoveInput : Vector2.zero;
         float inputX = moveInput.x;   // composante gauche/droite
         float inputY = moveInput.y;   // composante avant/arrière
 
         Vector3 currentEuler = transform.eulerAngles;
-        currentEuler.y = Camera.main.transform.eulerAngles.y;
+        if (_camera) currentEuler.y = _camera.transform.eulerAngles.y;
         transform.eulerAngles = currentEuler;
 
         // Calcule la direction de déplacement dans le plan horizontal, en tenant compte de l'orientation du joueur (qui suit la caméra)
@@ -30,15 +31,15 @@ public class MoveScript : MonoBehaviour
         moveDir.y = 0f;  // on reste sur le plan horizontal
 
         // Applique le déplacement soit via CharacterController, soit via la position du transform
-        if (controller != null)
+        if (_controller)
         {
             // Utilise CharacterController si présent (gère la physique de déplacement au sol automatiquement)
-            controller.SimpleMove(moveDir * moveSpeed);
+            _controller.SimpleMove(moveDir * moveSpeed);
         }
         else
         {
             // Déplacement simple du transform (pas de gestion de gravité ou collisions)
-            transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(moveDir * (moveSpeed * Time.fixedDeltaTime), Space.World);
         }
     }
 }
